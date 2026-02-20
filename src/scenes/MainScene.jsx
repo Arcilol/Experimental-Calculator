db99c6633c88f2a71aebbe202d79b90eee35b628
JuanIkarigit config --global user.name JuanIkari <j.arcila1@utp.edu.co>
2026-02-19 21:42:49 -0500

    Calculadora importada

---
+++ src/scenes/MainScene.jsx
@@ -0,0 +1,70 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { useEffect, useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export default function MainScene() {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x87ceeb);
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000,
+    );
+
+    camera.position.set(0, 2, 5);
+    camera.lookAt(0, 0, 0);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mountRef.current.appendChild(renderer.domElement);
+
+    camera.position.set(0, 1.5, 4);
+
+    // LUCES
+    const light = new THREE.DirectionalLight(0xffffff, 2);
+    light.position.set(5, 5, 5);
+    scene.add(light);
+
+    const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambient);
+
+    // LOADER
+    const loader = new GLTFLoader();
+    loader.load("/calculator.glb", (gltf) => {
+      const model = gltf.scene;
+      scene.add(model);
+
+      const box = new THREE.Box3().setFromObject(model);
+      const center = new THREE.Vector3();
+      box.getCenter(center);
+
+      model.position.sub(center);
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.enableDamping = true; // movimiento suave
+      controls.dampingFactor = 0.05;
+      controls.enableZoom = true; // permitir zoom
+
+      console.log("Modelo cargado");
+    });
+
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+    animate();
+
+    return () => {
+      mountRef.current.removeChild(renderer.domElement);
+    };
+  }, []);
+
+  return <div ref={mountRef} />;
+}
